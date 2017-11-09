af8d8460d7b623a63382bcd230b9bc6b1d8a8dfc
dalin sun <sundalin0512@outlook.com>
2017-11-09 12:29:01 +0800

    a

---
+++ StudentManagement/File.cpp
@@ -0,0 +1,11 @@
+#include "File.h"
+namespace Sdalin
+{
+	File::File()
+	{
+	}
+
+	File::~File()
+	{
+	}
+}
---
+++ StudentManagement/types.h
@@ -0,0 +1,11 @@
+#pragma once
+
+enum class ReturnType
+{
+	RET_SUCCESS = 0,
+	RET_ERROR,
+	RET_NOSPACE,
+	RET_ILLEGAL_INDEX,
+	RET_NO_HEAP_SPACE
+};
+
